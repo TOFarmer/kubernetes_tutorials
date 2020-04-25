6160dd476965096e7fb4559e1b9dc08c3db85e50
TOFarmer <thomas.farmer27@hotmail.co.uk>
2020-04-25 01:39:17 +0100

    Added port-forward and delete

--- fundamentals/pods/declarative_pod.sh
+++ fundamentals/pods/declarative_pod.sh
@@ -8,3 +8,32 @@ kubectl run my-nginx --image=nginx:alpine
 # name of the pod will be 'my-nginx' followed by random string which is a unique
 # ID of the pod
 kubectl get all
+
+# Then use the full pod name to delete the pod
+POD_NAME=`kubectl get pods -o name | grep my-nginx`
+echo $POD_NAME
+kubectl delete $POD_NAME
+
+# As the desired number of running pods of type my-nginx is 1, kubernetes will
+# restart the pod, which will be assigned a different name (and UID)
+NEW_POD_NAME=`kubectl get pods -o name | grep my-nginx`
+echo $NEW_POD_NAME
+
+# As with Docker containers, port forwarding has to be specified.
+# To enable port forwarding for the new pod that was automatically created, we
+# can use the port-forward command. This will map the localhost 8080 port to the
+# pod 80 port (which is defined within the nginx:alpine image). The & is
+# included because this command normally pauses the terminal.
+kubectl port-forward $NEW_POD_NAME 8080:80
+
+# You can now check that forwarding is valid using a browser, which will display
+# a welcome to nginx
+
+# It is important to note that the port option for kubectl run does not also
+# setup port forwarding - all this does is expose the specified port for the
+# container. So the nginx image is effectively setup so that the following
+# occurs:
+kubectl run my-nginx --image=nginx:alpine --port=80
+
+# Finally to delete a pod you must delete the deployment
+kubectl delete deployment my-nginx
